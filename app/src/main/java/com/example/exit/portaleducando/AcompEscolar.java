e81b7b3620fb62f17126f7c75e19b97123c1d2c6
Danrley <dkvanish@gmail.com>
2016-10-20 01:20:15 -0300

    Commit 04 - Vs 2.0
    Criação das Fragments do Acomp. Escolar onde serão implementados os gráficos de Barra e Pizza via Webview

--- app/src/main/java/com/example/exit/portaleducando/AcompEscolar.java
+++ app/src/main/java/com/example/exit/portaleducando/AcompEscolar.java
@@ -1,32 +1,36 @@
 package com.example.exit.portaleducando;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageButton;
 
 public class AcompEscolar extends AppCompatActivity {
-
-
+    ImageButton selecbarra;
+    ImageButton selecpizza;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acomp_escolar);
 
-        ImageButton selecbarra = (ImageButton) findViewById(R.id.selecbarra);
-        ImageButton selecpizza = (ImageButton) findViewById(R.id.selecpizza);
-        selecbarra.setOnClickListener(new View.OnClickListener() {
+        selecbarra = (ImageButton) findViewById(R.id.selecbarra);
+        selecpizza = (ImageButton) findViewById(R.id.selecpizza);
+
+             selecbarra.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getSupportFragmentManager().beginTransaction().add(R.id.container, new GraficoBarra()).commit();
+                getSupportFragmentManager().beginTransaction().add(R.id.fragmentContainer, new FragmentBarra()).commit();
             }
         });
 
         selecpizza.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getSupportFragmentManager().beginTransaction().add(R.id.container, new GraficoPizza()).commit();
+                getSupportFragmentManager().beginTransaction().add(R.id.fragmentContainer, new FragmentPizza()).commit();
             }
         });
     }
